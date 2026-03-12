1eb35a22bfc05b2d1b3986f0f744d80c163d524c
Lijin Durairaj <durairaj.lijin@gmail.com>
2026-03-12 16:57:13 +0530

    inital changes

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h3>hello world</h3>
+        <p>
+        welcome to the world of dockers
+        </p>
+
+        <p>
+          you are seeing real time changes in your machine
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
